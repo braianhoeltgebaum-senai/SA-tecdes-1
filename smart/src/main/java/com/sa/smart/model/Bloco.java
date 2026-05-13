33e422470318f2836030ce1191a408dfff77f01d
Eduardo Henrique Stahnke <eduardo_stahnke@estudante.sesisenai.org.br>
2026-05-13 19:49:53 -0300

    commit
    feito o repository e model do bloco

--- smart/src/main/java/com/sa/smart/model/Bloco.java
+++ smart/src/main/java/com/sa/smart/model/Bloco.java
@@ -1,5 +1,41 @@
 package com.sa.smart.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bloco")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bloco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idBloco;
+    
+    @ManyToOne
+    @JoinColumn(name = "pedido_ordem_producao", nullable = false)
+    private Pedido pedido;
+    
+    @ManyToOne
+    @JoinColumn(name = "estoque_posicao", nullable = false)
+    private Estoque estoque;
+    
+    @Column(name = "cor_bloco", nullable = false)
+    private Integer corBloco;
+    
+    @Column(name = "criado_em", nullable = false)
+    private LocalDateTime criadoEm;
+    
 }
